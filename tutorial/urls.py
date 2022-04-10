3d62927fbf0fa5df91a0dbe4311fecdc6118d637
DevLeti <devleti0106@gmail.com>
2022-04-10 14:27:42 +0900

    set environment

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,4 +1,4 @@
-"""drf_tutorial URL Configuration
+"""tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
